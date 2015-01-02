994d17aecae053e7dc1c7506acc72ddb332dba1a
dauchua <tqviet.hcmus@gmail.com>
2015-01-01 21:18:44 -0800

    Django part 4

---
+++ django-dauchua/django_test/django_test/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from article.views import HelloTemplate
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_test.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+	url(r'^hello/$', 'article.views.hello'),
+	url(r'^hello_template/$', 'article.views.hello_template'),
+	url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
+	url(r'^hello_class_template/$', HelloTemplate.as_view()),   	
+	url(r'^admin/', include(admin.site.urls)),
+)
